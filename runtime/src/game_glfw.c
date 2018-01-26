618731403cfa136b71ecae1ffbb491258ec48d91
Carlos Edmundo Martinez <ced.mendoza@gmail.com>
2018-01-26 12:37:52 -0600

    GLFW window configuration through desc file [feature]

--- runtime/src/game_glfw.c
+++ runtime/src/game_glfw.c
@@ -1,28 +1,61 @@
 #include <glad/glad.h>
 #define GLFW_INCLUDE_ES2
 #include <GLFW/glfw3.h>
+#include <wfe/asset.h>
+#include <wfe/pool.h>
+#include <wfe/desc.h>
 #include <wfe/game.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
+
+typedef struct wfeGameConfig {
+    wfeData *title;
+    wfeInt height;
+    wfeInt width;
+    wfeInt resizable;
+    wfeInt visible;
+    wfeInt decorated;
+    wfeInt focused;
+    wfeInt floating;
+    wfeInt maximized;
+} wfeGameConfig;
 
 typedef struct wfeGame {
     GLFWwindow *window;
     wfeError lastError;
+    wfePool miscPool;
 } wfeGame;
 
 // Callback to handle framebuffer resizes (i.e. from window resize)
 void wfeHandleFramebufferResize(GLFWwindow *window, GLint width, GLint height);
 
-wfeGame *wfeGameMake(wfeData *cname, wfeAny context) {
+// Dumps description file into configuration
+wfeError wfeConfigureGame(wfeGameConfig *config, const wfeChar *cname, wfePool *pool);
+
+wfeGame *wfeGameMake(wfeChar *cname, wfeAny context) {
     wfeGame *game = calloc(1, sizeof(wfeGame));
     if (game == NULL) {
         return NULL;
     }
 
-    assert(cname == NULL /* Not implemented yet */);
-
     // No errors at begining
     game->lastError = WFE_SUCCESS;
+
+    assert(cname != NULL /* Description file */);
+
+    // Init pool and game window configuration.
+    game->lastError = wfePoolInit(&game->miscPool);
+    if (WFE_HAVE_FAILED(game->lastError)) {
+        goto finalize;
+    }
+
+    // Configure game
+    wfeGameConfig config;
+    game->lastError = wfeConfigureGame(&config, cname, &game->miscPool);
+    if (WFE_HAVE_FAILED(game->lastError)) {
+        goto finalize;
+    }
 
     // Try to initialize glfw
     if(GL_FALSE == glfwInit()) {
@@ -34,11 +67,15 @@ wfeGame *wfeGameMake(wfeData *cname, wfeAny context) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    glfwWindowHint(GLFW_RESIZABLE, config.resizable);
+    glfwWindowHint(GLFW_VISIBLE, config.visible);
+    glfwWindowHint(GLFW_DECORATED, config.decorated);
+    glfwWindowHint(GLFW_FOCUSED, config.focused);
+    glfwWindowHint(GLFW_FLOATING, config.floating);
+    glfwWindowHint(GLFW_MAXIMIZED, config.maximized);
+
     // Default window creation.
-    game->window = glfwCreateWindow(
-            WFE_WINDOW_DEFAULT_WIDTH,
-            WFE_WINDOW_DEFAULT_HEIGHT,
-            WFE_WINDOW_DEFAULT_TITLE, NULL, NULL);
+    game->window = glfwCreateWindow(config.width, config.height, config.title, NULL, NULL);
     if(game->window == NULL) {
         game->lastError = WFE_GLFW_INIT_ERROR; goto finalize;
     }
@@ -60,6 +97,7 @@ finalize:
             glfwDestroyWindow(game->window);
         }
 
+        wfePoolFinalize(&game->miscPool);
         free(game);
     }
 
@@ -72,6 +110,7 @@ void wfeGameFinalize(wfeGame *game) {
         glfwDestroyWindow(game->window);
     }
 
+    wfePoolFinalize(&game->miscPool);
     glfwTerminate();
     free(game);
 }
@@ -83,3 +122,118 @@ wfeError wfeGameLastError(wfeGame *game) {
 void wfeHandleFramebufferResize(GLFWwindow *window, GLint width, GLint height) {
     glViewport(0, 0, width, height);
 }
+
+wfeError wfeConfigureGame(wfeGameConfig *config, const wfeChar *cname, wfePool *pool) {
+    wfeError code = WFE_SUCCESS;
+    const wfeChar *kname = NULL;
+    wfeSize ksize = 0L;
+
+    // Defaults
+    config->height = WFE_WINDOW_DEFAULT_HEIGHT;
+    config->width = WFE_WINDOW_DEFAULT_WIDTH;
+    config->title = WFE_WINDOW_DEFAULT_TITLE;
+    config->resizable = WFE_TRUE;
+    config->visible = WFE_TRUE;
+    config->decorated = WFE_TRUE;
+    config->focused = WFE_TRUE;
+    config->floating = WFE_FALSE;
+    config->maximized = WFE_FALSE;
+
+    wfeDesc desc;
+    wfeDescInit(&desc);
+
+    code = wfeAssetLoadDesc(cname, pool, &desc);
+    if (WFE_HAVE_FAILED(code)) {
+        return code;
+    }
+
+    code = wfeDescNextKey(&desc, &kname, &ksize);
+    while (WFE_SHOULD_CONTINUE(code)){
+        if (strncmp(kname, "height", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->height);
+            if (WFE_HAVE_FAILED(code)) {
+                config->height = WFE_WINDOW_DEFAULT_HEIGHT;
+                goto finalize;
+            }
+        }
+
+        if (strncmp(kname, "width", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->height);
+            if (WFE_HAVE_FAILED(code)) {
+                config->width = WFE_WINDOW_DEFAULT_WIDTH;
+                goto finalize;
+            }
+        }
+
+        if (strncmp(kname, "title", ksize) == 0) {
+            const wfeChar *tstr = NULL;
+            wfeSize tlen = 0L;
+
+            code = wfeDescGetString(&desc, &tstr, &tlen);
+            if (WFE_HAVE_FAILED(code)) {
+                config->title = WFE_WINDOW_DEFAULT_TITLE;
+                goto finalize;
+            }
+
+            // Perserve title's string on misc pool.
+            if (tlen > 0) {
+                config->title = wfePoolGet(pool, sizeof(char) * tlen + 1, wfeAlignOf(char));
+                strncpy(config->title, tstr, tlen);
+            } else {
+                config->title = WFE_WINDOW_DEFAULT_TITLE;
+            }
+        }
+
+        if (strncmp(kname, "resizable", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->resizable);
+            if (WFE_HAVE_FAILED(code)) {
+                config->resizable = WFE_TRUE;
+            }
+        }
+
+        if (strncmp(kname, "visible", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->visible);
+            if (WFE_HAVE_FAILED(code)) {
+                config->resizable = WFE_TRUE;
+            }
+        }
+
+        if (strncmp(kname, "decorated", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->decorated);
+            if (WFE_HAVE_FAILED(code)) {
+                config->resizable = WFE_TRUE;
+            }
+        }
+
+        if (strncmp(kname, "focused", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->focused);
+            if (WFE_HAVE_FAILED(code)) {
+                config->resizable = WFE_TRUE;
+            }
+        }
+
+        if (strncmp(kname, "floating", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->floating);
+            if (WFE_HAVE_FAILED(code)) {
+                config->resizable = WFE_FALSE;
+            }
+        }
+
+        if (strncmp(kname, "maximized", ksize) == 0) {
+            code = wfeDescGetInt(&desc, &config->maximized);
+            if (WFE_HAVE_FAILED(code)) {
+                config->resizable = WFE_FALSE;
+            }
+        }
+
+        code = wfeDescNextKey(&desc, &kname, &ksize);
+    }
+
+finalize:
+    wfeDescFinalize(&desc);
+    if (WFE_HAVE_FAILED(code))
+        return code;
+
+    return WFE_SUCCESS;
+}
+
--- runtime/tests/game_suite.c
+++ runtime/tests/game_suite.c
@@ -3,7 +3,7 @@
 
 static char * test_game_make_finalize() {
     // Try making a window and destroying it.
-    wfeGame *game = wfeGameMake(NULL, NULL);
+    wfeGame *game = wfeGameMake("test_game_glfw", NULL);
     mu_assert("game is null", game != NULL);
     mu_assert("there are errors at window creation", !WFE_HAS_FAILED(wfeGameLastError(game)));
     wfeGameFinalize(game);
@@ -11,6 +11,12 @@ static char * test_game_make_finalize() {
 }
 
 static char * game_suite() {
+    char *envsp = getenv("WFE_SEARCH_PATH");
+    if (envsp != NULL)
+        wfeAssetSetSearchPath(envsp);
+    else
+        wfeAssetSetSearchPath("tests/assets");
+
     mu_suite_start(game);
     mu_run_test(test_game_make_finalize);
     mu_suite_end(game);
